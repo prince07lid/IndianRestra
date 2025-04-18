3813efacb08ee33f936ab81f8ee22c5a28077cfe
Prince Lidhoriya <157975792+prince07lid@users.noreply.github.com>
2025-04-18 12:37:55 +0530

    Update Cartreducer.js

--- src/components/Cartreducer.js
+++ src/components/Cartreducer.js
@@ -1,45 +1,8 @@
-// import React,{createContext,useContext,useReducer} from 'react';
-
-// const cartStateContext=createContext();
-// const cartDispatchContext=createContext();
-
-// const reducer=(state,action)=>{
-//      switch(action.type){
-//         case "Add":
-//            return [...state, { name: action.name, price: action.price, size: action.size, quantity: action.quantity }];
-//         default:
-//            return state;   
-
-//      }
-// }
-
-// export const CartProvider=(({children})=>{
-//     const [state,dispatch]=useReducer(reducer,[]);
-//     return (
-//        <cartDispatchContext.Provider value={dispatch}>
-//         <cartStateContext.Provider value={state}>
-//             {children}
-//         </cartStateContext.Provider>
-//        </cartDispatchContext.Provider>
-        
-//     )
-// })
-// export const useStateCart = () => useContext(cartStateContext);
-// export const useDispatchCart = () => useContext(cartDispatchContext);
-
 import React, { createContext, useContext, useReducer } from "react";
 
 const cartStateContext = createContext();
 const cartDispatchContext = createContext();
 
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case "Add":
-//       return [...state, { id: action.id, name: action.name, price: action.price, size: action.size, quantity: action.quantity }];
-//     default:
-//       return state;
-//   }
-// };
 const reducer = (state, action) => {
     switch (action.type) {
       case "Add":
