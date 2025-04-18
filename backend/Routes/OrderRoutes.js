22ffd3d701a1a8a55d40eca26bb65153cec9a0e5
Prince Lidhoriya <157975792+prince07lid@users.noreply.github.com>
2025-04-18 12:42:09 +0530

    Update OrderRoutes.js

--- backend/Routes/OrderRoutes.js
+++ backend/Routes/OrderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../Models/Order"); // Import Order model
+const Order = require("../Models/Order");
 
 router.post("/placeorder", async (req, res) => {
     try {
